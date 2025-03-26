871fbbbddf34a3ddff13f449c593cf3e7a90d869
Imethma <imethmak@gmail.com>
2025-03-26 15:00:17 +0530

    Implement student form with submission and display functionality

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,11 +1,36 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { colors, Container, Paper } from '@mui/material';
+import { Button, colors, Container, Paper } from '@mui/material';
 
 export default function Student() {
     const paperStyle={padding:'50px 20px', width: 600, margin:"20px auto"}
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const[students,setStudents]=useState([])
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const student={name,address}
+        console.log(student)
+        fetch("http://localhost:8080/student/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(student)
+
+        }).then(()=>{
+            console.log("New student created")
+        })
+    }
     
+    useEffect(()=>{
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setStudents(result);
+        })
+    },[])
+
   return (
     <Container>
         <Paper elevation={3} style={paperStyle}>
@@ -17,12 +42,32 @@ export default function Student() {
       autoComplete="off"
     >
      
-      <TextField id="standard-basic" label="Student name" variant="standard" fullWidth/>
+      <TextField id="standard-basic" label="Student name" variant="standard" fullWidth 
+      value={name}
+      onChange={(e)=>setName(e.target.value)}/>
       
-      <TextField id="standard-basic" label="Student address" variant="standard" fullWidth/>
-      
+      <TextField id="standard-basic" label="Student address" variant="standard" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}/>
+      <Button variant='contained' color='secondary' onClick={handleClick}>Submit</Button>
     </Box>
     </Paper>
+
+    <h1>Students</h1>
+
+    <Paper elevation={3} styele={paperStyle}>
+
+        {students.map(student=>(
+            <Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={student.id}>
+                Id:{student.id}<br/>
+                Name:{student.name}<br/>
+                Address:{student.address}
+            </Paper>
+        ))
+        }
+
+    </Paper>
+
     </Container>
   );
 }
